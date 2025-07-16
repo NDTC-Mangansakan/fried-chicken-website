3229dc51001c19905191bcc021bd31c09cfa051f
Shaq <mangansakansansudin@gmail.com>
2025-07-17 07:46:07 +0800

    overlays and aos added

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -24,3 +24,35 @@ containerNavList.addEventListener('click', e => {
         containerNavList.classList.add('-right-[280px]')
     }
 })
+
+
+//AOS
+
+const sr = ScrollReveal({
+    delay: 300,
+    opacity:0,
+    distance:'100px',
+    origin: 'bottom',
+    // reset:true,
+    viewFactor:.4
+})
+
+// Main
+sr.reveal('.home-img-container', {origin: 'right', delay: 1800})
+sr.reveal('.home-data-text, .about-data-text', {interval:300})
+sr.reveal('.about-img-1', {origin: 'right', delay: 1500})
+sr.reveal('.about-img-2', {origin: 'left', delay: 1500})
+sr.reveal('.order-1-data', {origin: 'right', delay: 600})
+sr.reveal('.order-2-data', {origin: 'left', delay: 600})
+sr.reveal('.order-1-img', {origin: 'left', delay: 300})
+sr.reveal('.order-2-img', {origin: 'right', delay: 300})
+sr.reveal('.combo-lg-text-container')
+sr.reveal('.combo-img', {delay: 600})
+sr.reveal('.combo-num-container', {delay: 900})
+sr.reveal('.combo-data', {delay: 1200})
+sr.reveal('.contact-text, .about-data-text', {interval:300})
+sr.reveal('.contact-data-container', {origin: 'right', delay: 1500})
+
+// Overlays
+sr.reveal('.home-overlay', {interval:100, rotate:{z: 90}, delay: 1500})
+sr.reveal('.contact-overlay', {interval:100, rotate:{z:90}, delay:1500})
